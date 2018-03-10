bb7b5545bfc8db760ca7ce4e2522128bdad1fc61
Andy Mardell <andy@mardell.me>
2018-03-10 20:13:00 +0000

    Better help, better errors

--- index.js
+++ index.js
@@ -7,16 +7,28 @@ let dir
 
 program
   .usage('<dir>')
-  .description('If no argument is given, will show a chart for the current directory\n\n  Alternative usage:\n\n  dupie disk - show current disk usage\n  dupie <diskn> - show specific disk usage')
+  .description('If no argument is given, will show a chart for the current directory')
   .arguments('<dir>')
   .action(dirPath => {
     dir = dirPath
   })
+  .on('--help', () => {
+    console.log('')
+    console.log('  Examples:')
+    console.log('')
+    console.log('    $ dupie')
+    console.log('    $ dupie ~')
+    console.log('    $ dupie /path/to/dir')
+    console.log('    $ dupie disk')
+    console.log('    $ dupie disk1s1')
+    console.log('')
+  })
   .parse(process.argv)
 
-if (typeof dir === 'undefined' || dir.substring(0, 4) !== 'disk') {
-  const path = (typeof dir === 'undefined' ? '' : dir)
-  executeDu(path)
-} else {
+dir = (typeof dir === 'undefined' ? '' : dir)
+
+if (dir && dir.substring(0, 4) === 'disk') {
   executeDf(dir)
+} else {
+  executeDu(dir)
 }
